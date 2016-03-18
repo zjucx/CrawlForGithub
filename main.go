6040a30e860f2f8c56a7aa4c6bffc5377d2eb833
zjucx <zju.chx@gmail.com>
2016-03-18 23:05:22 +0800

    add json explain

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import(
   "net/http"
   "io/ioutil"
   "regexp"
+  "encoding/json"
+  "strings"
 )
 
 func main() {
@@ -18,12 +20,37 @@ func main() {
     fmt.Println("ioutil error is: ", err)
   }
   defer res.Body.Close()
+  bodystr := string(body)
   //reg := regexp.MustCompile(`fill="(.*)"`)
   reg := regexp.MustCompile("fill\\=\"[\\S\\s]+?\"")
   //reg := regexp.MustCompile("\\<[\\S\\s]+?\\>")
   //reg := regexp.MustCompile(`<rect (.*)`)
-  str := reg.FindAllString(string(body), -1)
-  str = reg.ReplaceAllString(str, -1)
-  //str := reg.FindAllString(string(body), -1)
-  fmt.Println(str)
+  tmp := reg.FindAllString(bodystr, -1)
+  startindex := 0
+  for index, color := range tmp{
+    if color != "fill=\"#eeeeee\"" {
+      startindex = index
+      break
+    }
+  }
+  colorstr := tmp[startindex:]
+
+  reg = regexp.MustCompile("data-count\\=\"[\\S\\s]+?\"")
+  tmp = reg.FindAllString(bodystr, -1)
+  countstr := tmp[startindex:]
+
+  reg = regexp.MustCompile("data-date\\=\"[\\S\\s]+?\"")
+  tmp = reg.FindAllString(bodystr, -1)
+  datestr := tmp[startindex:]
+
+  jsondata := make(map[string]interface{})
+  
+  for index, color := range colorstr{
+    jsondata["color"] = strings.Replace(strings.Replace(color, "fill=", "", -1), "\"", "", -1)
+    jsondata["count"] = strings.Replace(strings.Replace(countstr[index], "data-count=", "", -1), "\"", "", -1)
+    jsondata["date"] = strings.Replace(strings.Replace(datestr[index], "data-date=", "", -1), "\"", "", -1)
+  }
+   //str := reg.FindAllString(string(body), -1)
+   result, _ := json.Marshal(jsondata)
+  fmt.Println(string(result))
 }
