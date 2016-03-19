6feb4abbe8912b8b7071e4a15577f48d377e06e5
zjucx <zju.chx@gmail.com>
2016-03-19 19:20:40 +0800

    finish arrary to json and write string to json file

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import(
   "regexp"
   "encoding/json"
   "strings"
+  "os"
 )
+
+type GithubInfo struct {
+  Count string `json:"count"`
+  Date string `json:"date"`
+  Color string `json:"color"`
+}
 
 func main() {
   url := "https://github.com/zjucx"
@@ -43,14 +50,25 @@ func main() {
   tmp = reg.FindAllString(bodystr, -1)
   datestr := tmp[startindex:]
 
-  jsondata := make(map[string]interface{})
-  
+  githubArrary := make([]GithubInfo, 100)
+  //jsondata := make(map[string]interface{})
+
   for index, color := range colorstr{
-    jsondata["color"] = strings.Replace(strings.Replace(color, "fill=", "", -1), "\"", "", -1)
-    jsondata["count"] = strings.Replace(strings.Replace(countstr[index], "data-count=", "", -1), "\"", "", -1)
-    jsondata["date"] = strings.Replace(strings.Replace(datestr[index], "data-date=", "", -1), "\"", "", -1)
+    githubArrary[index].Color = strings.Replace(strings.Replace(color, "fill=", "", -1), "\"", "", -1)
+    githubArrary[index].Count = strings.Replace(strings.Replace(countstr[index], "data-count=", "", -1), "\"", "", -1)
+    githubArrary[index].Date = strings.Replace(strings.Replace(datestr[index], "data-date=", "", -1), "\"", "", -1)
   }
    //str := reg.FindAllString(string(body), -1)
-   result, _ := json.Marshal(jsondata)
-  fmt.Println(string(result))
+   result, _ := json.Marshal(githubArrary)
+   fmt.Println(string(result))
+   filename := "github.json"
+   githubFile, err := os.Create(filename)
+   if err != nil {
+     fmt.Println("Create file err:%s", err)
+   }
+   _, err = githubFile.WriteString(string(result))
+   if err != nil {
+     fmt.Println("Write file err:%s", err)
+   }
+   githubFile.Sync()
 }
